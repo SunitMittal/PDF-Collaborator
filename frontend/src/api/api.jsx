48077bb82b7e1037d21646998408ebb20888c9ad
SunitMittal <sunitmittal01@gmail.com>
2025-07-02 08:25:08 +0530

    made changes

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://spotdraft-1x72.onrender.com",
+  baseURL: "https://pdf-collaborator-smhg.onrender.com",
 });
 
 // Add request interceptor for tokens
